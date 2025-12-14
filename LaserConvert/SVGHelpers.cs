e80e45d87dd33ad66d7b909bc3c711d49b0bbd07
Jason MacEntee <jason@digitalmettle.com>
2025-12-14 09:25:12 -0500

    output should be in mm

--- LaserConvert/SVGHelpers.cs
+++ LaserConvert/SVGHelpers.cs
@@ -8,35 +8,42 @@ namespace LaserConvert
 {
     /// <summary>
     /// SVG builder for creating SVG documents with groups and paths.
+    /// Units are in millimeters for laser cutting applications.
     /// </summary>
     internal sealed class SvgBuilder
     {
         private readonly StringBuilder _sb = new StringBuilder();
+        private readonly StringBuilder _content = new StringBuilder();
         
         public SvgBuilder()
         {
-            _sb.AppendLine($"<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1000\" height=\"1000\" viewBox=\"0 0 1000 1000\">");
-            _sb.AppendLine("<defs/>");
+            // We'll build the header at the end when we know the content bounds
         }
         
         public void BeginGroup(string name)
         {
             name = Sanitize(name);
-            _sb.AppendLine($"  <g id=\"{name}\">");
+            _content.AppendLine($"  <g id=\"{name}\">");
         }
         
         public void EndGroup()
         {
-            _sb.AppendLine("  </g>");
+            _content.AppendLine("  </g>");
         }
         
         public void Path(string d, double strokeWidth, string fill, string stroke = "#000")
         {
-            _sb.AppendLine($"    <path d=\"{d}\" stroke=\"{stroke}\" stroke-width=\"{strokeWidth.ToString("0.###", CultureInfo.InvariantCulture)}\" fill=\"{fill}\" vector-effect=\"non-scaling-stroke\"/>");
+            _content.AppendLine($"    <path d=\"{d}\" stroke=\"{stroke}\" stroke-width=\"{strokeWidth.ToString("0.###", CultureInfo.InvariantCulture)}\" fill=\"{fill}\" vector-effect=\"non-scaling-stroke\"/>");
         }
         
         public string Build()
         {
+            // Use a large viewBox (1000x1000) but specify dimensions in mm
+            // The viewBox coordinates are unitless and represent the coordinate system
+            // The width/height with mm units tell Inkscape the physical size
+            _sb.AppendLine($"<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1000mm\" height=\"1000mm\" viewBox=\"0 0 1000 1000\">");
+            _sb.AppendLine("<defs/>");
+            _sb.Append(_content);
             _sb.AppendLine("</svg>");
             return _sb.ToString();
         }
