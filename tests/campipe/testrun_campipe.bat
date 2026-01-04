be9dba1a2761160f5206c5f7fe361b568e45c70a
Jason MacEntee <jason@digitalmettle.com>
2026-01-04 13:42:33 -0500

    cleanup test to get all objects output

--- tests/campipe/testrun_campipe.bat
+++ tests/campipe/testrun_campipe.bat
@@ -2,7 +2,7 @@ REM the correct way to run these tests is to execute this batch file from the co
 REM the correct way to read the test results is to read the contents of bb_results.txt after the test run is complete.
 
 
-"C:\Users\jdm\source\repos\Plasticity-LaserConvert\LaserConvert\bin\Debug\net10.0\LaserConvert.exe" "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\Cam Pipe Clamp Sliced.stp" "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\Cam Pipe Clamp Sliced.svg"  debugMode=true thickness=3 tolerance=2 > "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\CamPipe_output.txt"
+"C:\Users\jdm\source\repos\Plasticity-LaserConvert\LaserConvert\bin\Debug\net10.0\LaserConvert.exe" "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\Cam Pipe Clamp Sliced.stp" "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\Cam Pipe Clamp Sliced.svg"  debugMode=true thickness=7 tolerance=2 > "C:\Users\jdm\source\repos\Plasticity-LaserConvert\tests\campipe\CamPipe_output.txt"
 
 
 
